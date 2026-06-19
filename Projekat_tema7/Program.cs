71c0c1af9ec3d6c9201d7e8a4d2b53ed3cbf6d07
teodora <teodoravuckovic73@gmail.com>
2026-06-19 23:26:02 +0200

    crc32

--- Projekat_tema7/Program.cs
+++ Projekat_tema7/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Projekat_tema7.Detection;
+
+class Program
+{
+    static void Main()
+    {
+        CRC32 crcModule = new CRC32();
+        byte[] testData = System.Text.Encoding.UTF8.GetBytes("Ovo je test poruka");
+
+        uint checksum = crcModule.Compute(testData);
+
+        Console.WriteLine($"CRC-32 checksum: {checksum:X8}"); 
+    }
+}
