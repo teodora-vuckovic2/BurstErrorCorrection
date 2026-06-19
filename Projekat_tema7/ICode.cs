bdeae6b7bc6b02bbb4b0c26ae4f54e82cd0027ac
teodora <teodoravuckovic73@gmail.com>
2026-06-19 22:42:20 +0200

    Add project files.

---
+++ Projekat_tema7/ICode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_tema7
+{
+    public interface IErrorCorrection
+    {
+        byte[] Encode(byte[] data);
+        byte[] Decode(byte[] data);
+    }
+}
