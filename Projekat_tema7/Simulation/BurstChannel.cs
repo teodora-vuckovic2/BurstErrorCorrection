88b431cf82e611a7c481af7810b153451c83f81c
teodora <teodoravuckovic73@gmail.com>
2026-06-19 23:36:24 +0200

    BurstChannel

---
+++ Projekat_tema7/Simulation/BurstChannel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Projekat_tema7.Simulation
+{
+    public class BurstChannel
+    {
+        private Random _rng = new Random();
+        public double BurstProbability { get; set; } = 0.1; 
+        public int MaxBurstLength { get; set; } = 5; 
+
+        public byte[] ApplyNoise(byte[] data)
+        {
+            byte[] noisyData = (byte[])data.Clone();
+             
+            if (_rng.NextDouble() < BurstProbability)
+            {
+                int burstStart = _rng.Next(0, noisyData.Length);
+                int burstLength = _rng.Next(1, MaxBurstLength + 1);
+
+                for (int i = 0; i < burstLength; i++)
+                {
+                    int pos = (burstStart + i) % noisyData.Length; 
+                    noisyData[pos] ^= (byte)_rng.Next(1, 256);
+                }
+            }
+            return noisyData;
+        }
+    }
+}
